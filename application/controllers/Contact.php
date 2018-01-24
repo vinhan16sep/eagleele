588a2bd4852df1fffe79340b34d08d35eeb2bc51
vinhan16sep <vinhan16sep@gmail.com>
2018-01-24 14:49:49 +0700

    update contact management

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -32,21 +32,25 @@ class Contact extends Public_Controller {
         parse_str($this->input->post('input'), $params);
 
         $send = $this->send_mail($params);
-        $data = array(
-            'name' => $params['name'],
-            'email' => $params['email'],
-            'phone' => $params['phone'],
-            'reason' => $params['reason'],
-            'content' => $params['content'],
-            'created_at' => date('Y-m-d H:i:s')
-        );
-        $contact = $this->contact_model->insert($data);
 
-        if($send == false && $contact == false){
+        if(!$send){
             $this->output->set_status_header(404)
                 ->set_output(json_encode(array('message' => 'Fail', 'data' => $params)));
         }else{
+            $data = array(
+                'name' => $params['name'],
+                'email' => $params['email'],
+                'phone' => $params['phone'],
+                'reason' => $params['reason'],
+                'content' => $params['content'],
+                'created_at' => date('Y-m-d H:i:s')
+            );
+            $contact = $this->contact_model->insert($data);
 
+            if(!$contact){
+                $this->output->set_status_header(404)
+                    ->set_output(json_encode(array('message' => 'Fail', 'data' => $params)));
+            }
             $this->output->set_status_header(200)
                 ->set_output(json_encode(array('message' => 'Success', 'data' => $params)));
         }
